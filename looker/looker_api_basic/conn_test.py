f74a524765ba6b21daa2a06c35ccdb7dd33f1057
jr <jonreiter@gmail.com>
2024-02-05 13:46:07 +0800

    fix typo

--- looker/looker_api_basic/conn_test.py
+++ looker/looker_api_basic/conn_test.py
@@ -2,4 +2,4 @@ import looker_sdk
 sdk = looker_sdk.init40("looker.ini")
 boards = sdk.all_dashboards()
 for board in boards:
-    print(str(board.title + ' , ' + str(board.id)))
+    print(str(board.title) + ' , ' + str(board.id))
